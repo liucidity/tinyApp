4dbf84f0d628011ddd355c8402405f842c1facef
Travis <travisliu708@gmail.com>
2022-09-05 13:41:03 -0700

    connected view to server as get request. created string generator to add short URL. testing post input from form

--- express_server.js
+++ express_server.js
@@ -5,12 +5,30 @@ const PORT = 8080;
 
 app.set('view engine', 'ejs');
 
+const generateRandomString = function () {
+  let generatedShort = '';
+  const stringLength = 6
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+
+  for (let i = 0; i < stringLength; i++) {
+    generatedShort += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return generatedShort;
+}
+
+// SETUP MIDDLEWARE
 app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
+//ROUTING
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -24,13 +42,8 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
-app.get('/set', (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get('/fetch', (req, res) => {
-  res.send(`a = ${a}`);
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
 app.get('/urls', (req, res) => {
@@ -41,6 +54,11 @@ app.get('/urls', (req, res) => {
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render('urls_show', templateVars);
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('OK');
 });
 
 app.listen(PORT, () => {
