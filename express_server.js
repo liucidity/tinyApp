8d28769fbeef19bc6fb707836abcf2053c435aaa
Travis <travisliu708@gmail.com>
2022-09-05 14:03:44 -0700

    completed the post request to add form data to our database. added redirects after form submission, and redirects to longURL with /u/ router

--- express_server.js
+++ express_server.js
@@ -54,11 +54,19 @@ app.get('/urls', (req, res) => {
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render('urls_show', templateVars);
+  res.redirect(urlDatabase[req.params.id]);
+});
+
+app.get('/u/:id', (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
 app.post('/urls', (req, res) => {
-  console.log(req.body);
-  res.send('OK');
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+
 });
 
 app.listen(PORT, () => {
