c45ab2bc07d5b7689843595a4cdba0a2b26e0b51
Travis <travisliu708@gmail.com>
2022-09-06 10:01:18 -0700

    tested account access security with curl post methods

--- express_server.js
+++ express_server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
-const { redirect } = require('express/lib/response');
 const app = express();
 const PORT = 8080;
 
@@ -25,16 +24,31 @@ const findEmail = function (registrationEmail) {
   //retuns entire user object if email is found or return null
   for (const user in users) {
     if (registrationEmail === users[user].email) {
-      console.log(users[user].email);
       return users[user];
     }
   }
   return null;
 };
 
+const urlsForUserID = function (userID) {
+  let urls = {};
+  for (const url in urlDatabase) {
+    if (userID === urlDatabase[url].userID) {
+      urls[url] = urlDatabase[url].longURL;
+    }
+  }
+  return urls;
+};
+
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "userRandomID",
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "userRandomID",
+  }
 };
 
 const users = {
@@ -44,7 +58,7 @@ const users = {
     password: "1234",
   },
   user2RandomID: {
-    id: "uder2RandomID",
+    id: "user2RandomID",
     email: "b@b.com",
     password: "4321",
   },
@@ -91,20 +105,41 @@ app.get('/urls/new', (req, res) => {
 
 // ALL CREATED URLS
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase, user: users[req.cookies['userID']] };
-  res.render('urls_index', templateVars);
+  if (!req.cookies['userID']) {
+    const templateVars = { urls: urlDatabase, user: users[req.cookies['userID']] };
+    res.render('urls_index', templateVars);
+  } else {
+    const templateVars = { urls: urlsForUserID(req.cookies['userID']), user: users[req.cookies['userID']] };
+    console.log(urlsForUserID(req.cookies['userID']));
+    res.render('urls_index', templateVars);
+  }
 });
 
 // INDIVIDUAL URL / EDITING
 app.get('/urls/:id', (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user: users[req.cookies['userID']] }; //wtvr id in our /:id route is passed into our object through req.params.key (key being id due to :id)
+  console.log(req.params.id);
+
+  // TODO: if random urls/id is entered it cannot read database
+  if (!(Object.keys(urlDatabase).includes(req.params.id))) {
+    res.status(400).send('This URL has not been created yet');
+  } else {
+    if (req.cookies['userID'] !== urlDatabase[`${req.params.id}`].userID) {
+      // TODO: change page to not send HTML only
+      res.status(400).send("You do not own this URL.");
+
+    }
+
+
+  }
+
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], urls: urlDatabase, user: users[req.cookies['userID']] }; //wtvr id in our /:id route is passed into our object through req.params.key (key being id due to :id)
   res.render('urls_show', templateVars);
 });
 
 // REDIRECTION TO LONGURL
 app.get('/u/:id', (req, res) => {
   if (!urlDatabase[req.params.id]) {
-    res.status(400).send("shortened URL does not exist")
+    res.status(400).send("shortened URL does not exist");
   }
 
 
@@ -153,29 +188,57 @@ app.post('/logout', (req, res) => {
   res.redirect('/urls');
 });
 
-// CREATES NEW URL
-app.post('/urls', (req, res) => {
-  if (!req.cookies['userID']) {
-    res.status(400).send("Please Login or register to create TinyURL");
+// UPDATE URL ENTRY
+app.post('/urls/:id', (req, res) => {
+
+  if (!Object.keys(urlDatabase).includes(req.params.id)) {
+    res.status(400).send("This URL does not exist");
   }
 
-  const shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`/urls`);
+  if (req.cookies['userID'] !== urlDatabase[req.params.id].userID) {
+    res.status(400).send("You do not have access to this URL");
+  } else {
+
+    urlDatabase[req.params.id].longURL = req.body.updateLongURL;
+    res.redirect('/urls');
+  }
 
 });
 
 // DELETE URL ENTRY
 app.post('/urls/:id/delete', (req, res) => {
-  delete urlDatabase[req.params.id];
-  res.redirect('/urls');
+  if (!Object.keys(urlDatabase).includes(req.params.id)) {
+    res.status(400).send("This URL does not exist");
+  }
+
+  if (req.cookies['userID'] !== urlDatabase[`${req.params.id}`].userID) {
+    res.status(400).send("You do not have permission to delete this URL");
+  } else {
+
+    delete urlDatabase[req.params.id];
+    res.redirect('/urls');
+  }
+
 });
 
-// UPDATE URL ENTRY
-app.post('/urls/:id', (req, res) => {
-  urlDatabase[req.params.id] = req.body.updateLongURL;
-  res.redirect('/urls');
+
+// CREATES NEW URL
+app.post('/urls', (req, res) => {
+
+  if (!req.cookies['userID']) {
+    res.status(400).send("Please Login or register to create TinyURL");
+  } else {
+
+    const shortURL = generateRandomString();
+    urlDatabase[shortURL] = { longURL: req.body.longURL, userID: req.cookies['userID'] };
+    res.redirect(`/urls`);
+
+  }
+
+
 });
+
+
 
 
 // ------------------------------------------------- PORT LISTEN
