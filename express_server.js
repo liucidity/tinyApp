4639456e17e187532ab9c06cc0fd666b6b326c3c
Travis <travisliu708@gmail.com>
2022-09-05 19:50:26 -0700

    added basic registration form ,and display email instead of userID in navbar

--- express_server.js
+++ express_server.js
@@ -6,6 +6,7 @@ const PORT = 8080;
 
 app.set('view engine', 'ejs');
 
+// ------------------------------------------------- GLOBALS
 const generateRandomString = function () {
   let generatedShort = '';
   const stringLength = 6;
@@ -19,15 +20,28 @@ const generateRandomString = function () {
   return generatedShort;
 };
 
-// ------------------------------------------------- SETUP MIDDLEWARE
-app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "a@a.com",
+    password: "1234",
+  },
+  user2RandomID: {
+    id: "uder2RandomID",
+    email: "b@b.com",
+    password: "4321",
+  },
+};
+
+// ------------------------------------------------- SETUP MIDDLEWARE
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 
 // ------------------------------------------------- ROUTING
@@ -46,17 +60,17 @@ app.get('/register', (req, res) => {
 });
 
 app.get('/urls/new', (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
-  res.render('urls_new');
+  const templateVars = { username: req.cookies["username"], user: users[req.cookies['userID']] };
+  res.render('urls_new', templateVars);
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"], user: users[req.cookies['userID']] };
   res.render('urls_index', templateVars);
 });
 
 app.get('/urls/:id', (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] }; //wtvr id in our /:id route is passed into our object through req.params.key (key being id due to :id)
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"], user: users[req.cookies['userID']] }; //wtvr id in our /:id route is passed into our object through req.params.key (key being id due to :id)
   res.render('urls_show', templateVars);
 });
 
@@ -66,6 +80,14 @@ app.get('/u/:id', (req, res) => {
 });
 
 // WRITES
+app.post('/register', (req, res) => {
+  let userID = generateRandomString();
+  let email = req.body.email;
+  let password = req.body.password;
+  users[userID] = { id: userID, email: email, password: password };
+  res.cookie('userID', userID);
+  res.redirect('/urls');
+});
 app.post('/urls', (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
@@ -84,12 +106,12 @@ app.post('/urls/:id', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  res.cookie('username', req.body.username);
+  res.cookie('userID', req.body.userID);
   res.redirect('/urls');
 });
 
 app.post('/logout', (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('userID');
   res.redirect('/urls');
 });
 
